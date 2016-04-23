902e7b9b287a98dd5c4051ba7fc8623bb60bb9c1
Sanjay Singh <itssanjay@gmail.com>
2016-04-23 15:02:31 -0400

    static files

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('static'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.render('static/index.html');
 });
 
 app.listen(app.get('port'), function() {
